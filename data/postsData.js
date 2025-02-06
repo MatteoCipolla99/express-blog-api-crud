59cfeb7a771bd85bdb6d9159354bc0458d11bf05
Matteo Cipolla <mati_teo@live.it>
2025-02-06 16:36:55 +0100

    add:fix image

--- data/postsData.js
+++ data/postsData.js
@@ -4,7 +4,7 @@ const posts = [
     title: "Viaggio in Giappone: un'avventura indimenticabile",
     content:
       "Scopri le bellezze del Giappone, tra tradizione e modernità. Dai templi di Kyoto ai grattacieli di Tokyo.",
-    image: "/img/ciambellone.jpeg",
+    image: "http://localhost:3000/img/ciambellone.jpeg",
     tags: ["viaggi", "Giappone", "avventura", "cultura"],
   },
   {
@@ -12,7 +12,7 @@ const posts = [
     title: "Le migliori ricette per una cena veloce",
     content:
       "Hai poco tempo? Prova queste ricette rapide e gustose per stupire i tuoi ospiti senza stress.",
-    image: "/img/cracker_barbabietola.jpeg",
+    image: "http://localhost:3000/img/cracker_barbabietola.jpeg",
     tags: ["cucina", "ricette veloci", "cena", "gastronomia"],
   },
   {
@@ -20,7 +20,7 @@ const posts = [
     title: "Tecnologie emergenti del 2025",
     content:
       "Un'analisi delle tecnologie più innovative che stanno rivoluzionando il nostro futuro.",
-    image: "/img/pasta_barbabietola.jpeg",
+    image: "http://localhost:3000/img/pasta_barbabietola.jpeg",
     tags: ["tecnologia", "innovazione", "futuro", "startup"],
   },
   {
@@ -28,7 +28,7 @@ const posts = [
     title: "10 libri da leggere almeno una volta nella vita",
     content:
       "Una lista di capolavori letterari che ogni appassionato di lettura dovrebbe conoscere.",
-    image: "/img/pane_fritto_dolce.jpeg",
+    image: "http://localhost:3000/img/pane_fritto_dolce.jpeg",
     tags: ["libri", "lettura", "cultura", "classici"],
   },
   {
@@ -36,7 +36,7 @@ const posts = [
     title: "Allenamento a casa: guida per principianti",
     content:
       "Non hai tempo per la palestra? Ecco un programma di allenamento semplice da fare a casa.",
-    image: "/img/torta_paesana.jpeg",
+    image: "http://localhost:3000/img/torta_paesana.jpeg",
     tags: ["fitness", "allenamento", "salute", "casa"],
   },
 ];
